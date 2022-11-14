7cc9734a74edb78daf3dc56dc978bc6270ff8500
Taiyoung Jang <taiyoung.jang@gmail.com>
2022-11-14 11:56:27 +0900

    rust enum만 include

--- TableGenerateCmd.NetCore/ExportToRust.cs
+++ TableGenerateCmd.NetCore/ExportToRust.cs
@@ -44,7 +44,7 @@ namespace TableGenerate
                         writer.WriteLine("use binary_reader::{BinaryReader, Endian};");
                         writer.WriteLineEx("use flate2::read::ZlibDecoder;");
                         writer.WriteLineEx("use crate::tbl::lib;");
-                        writer.WriteLineEx("include!(concat!(env!(\"OUT_DIR\"), concat!(\"/\", \"_\", \".rs\")));");
+                        writer.WriteLineEx("include!(\"_.rs\");");
                         string[] sheets = imp.GetSheetList();
 
                         filename = filename.Replace(".rs", string.Empty);
